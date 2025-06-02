33168e3c9dc68b6d772e78fa8b7afd5da8c3df82
sofia rigueiro <sofiarigueiro@eaportal.org>
2025-06-02 00:15:28 -0300

    criacao da funcao addHideWarningClass para dar funcao ao botao X que esta na div warning

--- main.js
+++ main.js
@@ -71,3 +71,7 @@ function checkItem(itemName) {
     item.checked = !item.checked
     showItemsList()
 }
+
+function addHideWarningClass() { 
+    document.querySelector(".warning").classList.add("hide-warning")
+}
